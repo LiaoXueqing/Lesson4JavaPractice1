4b04cf0789216ed325daf7202a7a7a079d15d7cb
xuexue <1047504737@qq.com>
2018-04-25 09:55:12 +0800

    test coding

--- src/test/java/com/tw/LibraryTest.java
+++ src/test/java/com/tw/LibraryTest.java
@@ -1,21 +1,16 @@
 package com.tw;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class LibraryTest {
-    @Mock
-    private StudentInfo stu;
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(stu);
-    }
 
     @Test
     public void testSomeLibraryMethod() {
@@ -23,7 +18,7 @@ public class LibraryTest {
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
 
-/*    @Test
+    @Test
     public void testMockClass() throws Exception {
         // you can mock concrete classes, not only interfaces
         LinkedList mockedList = mock(LinkedList.class);
@@ -34,15 +29,44 @@ public class LibraryTest {
 
         assertEquals(mockedList.get(0), value);
 
-    }*/
-    @Test
-    public void testAddStudent() {
-        when(stu.getNumber()).thenReturn("321");
-        assertEquals(stu.getNumber(),"321");
     }
     @Test
-    public void testPrintScores() {
+    public void testAddStudentRightResults() {
+        String s1 = "张, 321, 数学：90, 语文：88, 英语：86, 编程：88";
+        StudentInfo stu1 = new StudentInfo("张","321",90,88,86,88);
+        Library classUnderTest = new Library();
+        StudentInfo stu2 = classUnderTest.formatStudentInfo(s1);
+        assertEquals(stu1.getNumber(),stu2.getNumber());
+    }
+    @Test
+    public void testAddStudentErrorResults(){
+        String s1 = "张, 321, 数学：90";
+        Library classUnderTest = new Library();
+        StudentInfo stu = classUnderTest.formatStudentInfo(s1);
+        assertThat(stu, equalTo(null));
+    }
 
+    @Test
+    public void testPrintScoresRightResults() {
+        //张, 321, 数学：90, 语文：88, 英语：86, 编程：88
+        //王, 322, 数学：78, 语文：90, 英语：86, 编程：96
+        StudentInfo stu1 = new StudentInfo("张","321",90,88,86,88);
+        StudentInfo stu2 = new StudentInfo("王","322",78,90,86,96);
+        Library.student_list.add(stu1);
+        Library.student_list.add(stu2);
+
+        String input = "321,322";
+        Library classUnderTest = new Library();
+        List<StudentInfo> studentInfos = classUnderTest.formatStudentNos(input);
+        assertEquals(2,studentInfos.size());
+    }
+
+    @Test
+    public void testPrintScoresErrorResults(){
+        String input = "学号：321,学号：322";
+        Library classUnderTest = new Library();
+        List<StudentInfo> studentInfos = classUnderTest.formatStudentNos(input);
+        assertTrue(studentInfos.isEmpty());
     }
 
 }
